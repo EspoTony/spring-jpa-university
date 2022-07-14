4ac1e6cda5ee296032e1293d19d4a7d62851f8eb
EspoTony <espotony98@gmail.com>
2022-07-14 15:44:17 +0200

    esercizio finito?

---
+++ spring-jpa-university/src/main/java/jana60/controller/MainController.java
@@ -0,0 +1,29 @@
+package jana60.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jana60.repository.*;
+import jana60.model.*;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+	@GetMapping
+	public String countries(Model model) {
+		List<String> dep = List.of("Ingegneria", "Architettura", "Economia");
+		model.addAttribute("depar", dep);
+		return "index";
+	}
+
+	@Autowired
+	private DepartmentsRepository repo;
+}
