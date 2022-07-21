455ce4a30caf6e3540ba939482be6751bb337e24
EspoTony <105849690+EspoTony@users.noreply.github.com>
2022-07-21 15:08:44 +0200

    Delete spring-pizza directory

--- spring-pizza/src/main/java/jana60/repository/PizzaRepository.java
+++
@@ -1,7 +0,0 @@
-package jana60.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import jana60.model.Pizza;
-
-public interface PizzaRepository extends CrudRepository<Pizza , Integer> {
-}
